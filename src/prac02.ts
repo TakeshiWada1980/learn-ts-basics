98f67baa8a70b824d02ec5634a691b4b6430bf87
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-10-09 10:34:21 +0900

    update 2 files, create 1 file and delete 1 file

--- src/prac02.ts
+++ src/prac02.ts
@@ -1,19 +1,6 @@
-export {};
-import { Todo } from "./types"; // 型定義の読込み
-
-const todo1: Todo = {
+const todo = {
   name: "TypeScriptの勉強",
   priority: 3,
   isDone: false,
   deadline: new Date(2024, 9, 11, 9, 45),
 };
-
-const todo2: Todo = {
-  name: "基礎物理3の宿題",
-  priority: 1,
-  isDone: false,
-  deadline: new Date(2024, 9, 10, 16, 0),
-};
-
-console.log(JSON.stringify(todo1, null, 2));
-console.log(JSON.stringify(todo2, null, 2));
